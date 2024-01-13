11614095e916c6f59ea6811508f208d42d433579
heitor2002 <montagnini.heitor@gmail.com>
2024-01-13 14:15:41 -0300

    feat: estilização de header.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
@@ -6,14 +7,13 @@ export default function Header() {
       <div className="container">
         <nav className="flex justify-between">
           <div className="text-white grid grid-cols-4 gap-5">
-            <Link href={"/"}>Página Inicial</Link>
-            <Link href={"/"}>Produtos</Link>
-            <Link href={"/"}>Projetos</Link>
-            <Link href={"/"}>Quem somos</Link>
+            <Link href={"/"} className="text-md font-bold hover:text-orange-600 duration-300">Página Inicial</Link>
+            <Link href={"/"} className="text-md font-bold hover:text-orange-600 duration-300">Produtos</Link>
+            <Link href={"/"} className="text-md font-bold hover:text-orange-600 duration-300">Projetos</Link>
+            <Link href={"/"} className="text-md font-bold hover:text-orange-600 duration-300">Quem somos</Link>
           </div>
           <div className="text-white">
-
-          <Link href={""}>Entrar em contato</Link>
+            <Link href={"/contato"} className="border border-white hover:border-orange-600 hover:bg-orange-600 py-2 px-5 rounded-lg duration-200">Entrar em contato</Link>
           </div>
         </nav>
       </div>
