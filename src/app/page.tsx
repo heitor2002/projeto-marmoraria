513e7da1e3ded497057c8b57f3bdef635b32e7d6
heitor2002 <montagnini.heitor@gmail.com>
2024-01-13 18:59:28 -0300

    feat: finalizando segunda metade da sessão 1.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,13 @@ export default function Home() {
           <p className="mt-10 text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat impedit facere, qui at eveniet assumenda dignissimos non eaque aliquam neque aliquid consequatur quam itaque cupiditate minus, dolore laboriosam voluptates hic.</p>
           <Link href={"/"} className="py-3 border border-emerald-500 text-center max-w-xs font-bold bg-emerald-500 text-white rounded-lg duration-300 text-md mt-5 hover:scale-105">Entrar em contato via Whatsapp</Link>
         </div>
+        <div>
+          <div className="w-full h-full flex justify-end items-center">
+            <div className="h-72 back-image">
+              <img className="w-full max-w-md h-full object-cover rounded-2xl shadow-lg" src="https://png.pngtree.com/thumb_back/fw800/background/20230717/pngtree-contemporary-white-living-room-with-luxurious-marble-flooring-and-sleek-white-image_3895769.jpg"/>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
