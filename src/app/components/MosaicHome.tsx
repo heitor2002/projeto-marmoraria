1d7a77f29b1e47f124a330e1632992a3b85fc979
heitor2002 <montagnini.heitor@gmail.com>
2024-01-14 11:45:22 -0300

    feat: sessão de galeria.

---
+++ src/app/components/MosaicHome.tsx
@@ -0,0 +1,112 @@
+export default function MosaicHome(){
+    return(
+        <div className="grid grid-cols-3 gap-10 mt-10">
+            <div className="w-full p-5 border-b-2 border-l-2 border-white rounded-lg flex justify-center items-center gap-5 text-white font-bold text-2xl hover:text-orange-600 hover:border-orange-600 hover:-translate-y-3 duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+                />
+              </svg>
+              <h2>Atendimento</h2>
+            </div>
+            <div className="w-full p-5 border-b-2 border-white rounded-lg flex justify-center items-center gap-5 text-white font-bold text-2xl hover:text-orange-600 hover:border-orange-600 hover:-translate-y-3 duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+                />
+              </svg>
+
+              <h2>Criatividade</h2>
+            </div>
+            <div className="w-full p-5 border-b-2 border-r-2 border-white rounded-lg flex justify-center items-center gap-5 text-white font-bold text-2xl hover:text-orange-600 hover:border-orange-600 hover:-translate-y-3 duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                />
+              </svg>
+
+              <h2>Produtos de qualidade</h2>
+            </div>
+            <div className="w-full p-5 border-b-2 border-l-2 border-white rounded-lg flex justify-center items-center gap-5 text-white font-bold text-2xl hover:text-orange-600 hover:border-orange-600 hover:-translate-y-3 duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+
+              <h2>Profissionais qualificados</h2>
+            </div>
+            <div className="w-full p-5 border-b-2 border-white rounded-lg flex justify-center items-center gap-5 text-white font-bold text-2xl hover:text-orange-600 hover:border-orange-600 hover:-translate-y-3 duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+
+              <h2>Pontualidade</h2>
+            </div>
+            <div className="w-full p-5 border-b-2 border-r-2 border-white rounded-lg flex justify-center items-center gap-5 text-white font-bold text-2xl hover:text-orange-600 hover:border-orange-600 hover:-translate-y-3 duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+                />
+              </svg>
+              <h2>Ótiomo custo-benefício</h2>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,69 @@
-import Image from "next/image"
-import logo from "../../public/logo.png"
-import Link from "next/link"
-import SliderHome from "./components/SliderHome"
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import Link from "next/link";
+import SliderHome from "./components/SliderHome";
+import MosaicHome from "./components/MosaicHome";
 
 export default function Home() {
   return (
     <div>
-
-    <div className="container max-w-6xl">
-      <main className="grid grid-cols-2 py-20">
-        <div className="flex flex-col gap-3">
-          <Image alt="logo" src={logo}/>
-          <p className="mt-10 text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat impedit facere, qui at eveniet assumenda dignissimos non eaque aliquam neque aliquid consequatur quam itaque cupiditate minus, dolore laboriosam voluptates hic.</p>
-          <Link href={"/"} className="py-3 border border-emerald-500 text-center max-w-xs font-bold bg-emerald-500 text-white rounded-lg duration-300 text-md mt-5 hover:scale-105">Entrar em contato via Whatsapp</Link>
-        </div>
-        <div>
-          <div className="w-full h-full flex justify-end items-center">
-            <div className="h-72 back-image">
-              <img className="w-full max-w-md h-full object-cover rounded-2xl shadow-lg" src="https://png.pngtree.com/thumb_back/fw800/background/20230717/pngtree-contemporary-white-living-room-with-luxurious-marble-flooring-and-sleek-white-image_3895769.jpg"/>
+      <div className="container max-w-6xl">
+        <main className="grid grid-cols-2 py-20">
+          <div className="flex flex-col gap-3">
+            <Image alt="logo" src={logo} />
+            <p className="mt-10 text-md">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+              impedit facere, qui at eveniet assumenda dignissimos non eaque
+              aliquam neque aliquid consequatur quam itaque cupiditate minus,
+              dolore laboriosam voluptates hic.
+            </p>
+            <Link
+              href={"/"}
+              className="py-3 border border-emerald-500 text-center max-w-xs font-bold bg-emerald-500 text-white rounded-lg duration-300 text-md mt-5 hover:scale-105"
+            >
+              Entrar em contato via Whatsapp
+            </Link>
+          </div>
+          <div>
+            <div className="w-full h-full flex justify-end items-center">
+              <div className="h-72 back-image">
+                <img
+                  className="w-full max-w-md h-full object-cover rounded-2xl shadow-lg"
+                  src="https://png.pngtree.com/thumb_back/fw800/background/20230717/pngtree-contemporary-white-living-room-with-luxurious-marble-flooring-and-sleek-white-image_3895769.jpg"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
       <section className="py-10">
         <div className="container">
-          <h2 className="text-center text-3xl">Transformando <span className="font-bold text-orange-600">Ideias </span>em <span className="font-bold text-orange-600">Elegância</span>, cada pedra conta uma <span className="font-bold text-orange-600">História.</span></h2>
+          <h2 className="text-center text-3xl">
+            Transformando{" "}
+            <span className="font-bold text-orange-600">Ideias </span>em{" "}
+            <span className="font-bold text-orange-600">Elegância</span>, cada
+            pedra conta uma{" "}
+            <span className="font-bold text-orange-600">História.</span>
+          </h2>
           <SliderHome />
         </div>
       </section>
+      <section className="py-10 bg-zinc-950">
+        <div className="container max-w-5xl">
+          <h2 className="text-white text-2xl font-thin">
+            Especializada em{" "}
+            <span className="italic font-bold text-orange-600">
+              revestimentos
+            </span>{" "}
+            de túmulos em granito, galerias e construções
+          </h2>
+          <div className="mx-auto my-10 bg-orange-600 w-full max-w-40 h-1"></div>
+          <h2 className="text-orange-600 text-center text-3xl font-bold tracking-widest">
+            Somos referência no mercado
+          </h2>
+          <MosaicHome />
+        </div>
+      </section>
     </div>
-  )
+  );
 }
