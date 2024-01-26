0ae2b4f768138c1c82e80855d4dedce15e452f08
heitor2002 <montagnini.heitor@gmail.com>
2024-01-25 22:02:52 -0300

    feat: responsividade mosaico.

--- src/app/components/Mosaic.tsx
+++ src/app/components/Mosaic.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Mosaic() {
   return (
     <article className="pt-10 pb-20 bg-zinc-900">
-      <div className="container max-w-4xl">
+      <div className="container md:max-w-4xl max-w-md">
         <h2 className="text-zinc-100 text-2xl font-bold">
           Produtos e serviços de excelência
         </h2>
         <div className="w-full max-w-44 h-1 bg-orange-600 mt-5"></div>
-        <div className="w-full grid grid-cols-2 gap-5 mt-10">
+        <div className="w-full md:grid md:grid-cols-2 flex flex-col gap-5 mt-10">
           <div className="w-full h-60">
             <img
               src="https://www.cliquearquitetura.com.br/public/Painel/midia/imagem/full/bancadas_de_cozinha_em_granito_preto_absoluto_6218.jpg"
@@ -16,21 +16,21 @@ export default function Mosaic() {
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="w-full px-4">
+          <div className="w-full px-4 md:text-left text-center">
             <h2 className="text-zinc-100 text-2xl font-bold">Produtos</h2>
-            <div className="w-24 h-1 bg-orange-400 mt-3"></div>
-            <p className="text-md text-zinc-50 font-thin mt-3 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, vitae quaerat. At adipisci facere nesciunt, sunt quasi nihil eligendi amet velit possimus pariatur autem vitae, rerum voluptatem temporibus est sed.</p>
+            <div className="w-24 h-1 bg-orange-400 mt-3 md:mx-0 mx-auto"></div>
+            <p className="text-md text-zinc-50 font-thin mt-3 md:mb-5 mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, vitae quaerat. At adipisci facere nesciunt, sunt quasi nihil eligendi amet velit possimus pariatur autem vitae, rerum voluptatem temporibus est sed.</p>
             <Link href={"/"} className="border border-orange-500 py-3 px-5 rounded-md text-orange-500 hover:bg-orange-500 hover:text-zinc-50 duration-300">Visualizar produtos</Link>
           </div>
-          <div className="w-full px-4 text-right">
-            <h2 className="text-zinc-100 text-2xl font-bold">Trabalhos</h2>
-            <div className="mt-3 flex justify-end">
+          <div className="w-full px-4 md:text-right text-center md:order-3 order-4">
+            <h2 className="text-zinc-100 text-2xl font-bold ">Trabalhos</h2>
+            <div className="mt-3 flex md:justify-end justify-center">
               <div className="w-24 h-1 bg-orange-400"></div>
             </div>
-            <p className="text-md text-zinc-50 font-thin mt-3 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, vitae quaerat. At adipisci facere nesciunt, sunt quasi nihil eligendi amet velit possimus pariatur autem vitae, rerum voluptatem temporibus est sed.</p>
+            <p className="text-md text-zinc-50 font-thin mt-3 md:mb-5 mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, vitae quaerat. At adipisci facere nesciunt, sunt quasi nihil eligendi amet velit possimus pariatur autem vitae, rerum voluptatem temporibus est sed.</p>
             <Link href={"/"} className="border border-orange-500 py-3 px-5 rounded-md text-orange-500 hover:bg-orange-500 hover:text-zinc-50 duration-300">Visualizar produtos</Link>
           </div>
-          <div className="w-full h-60">
+          <div className="w-full h-60 md:order-4 order-3 md:mt-0 mt-10">
             <img
               src="https://www.marmorariamasterpedras.com.br/wp-content/uploads/2023/01/Marmoraria-Feira-das-Pedras-Marmores-e-Granitos.jpg"
               alt=""
