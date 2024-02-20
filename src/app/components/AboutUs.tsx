ad0f7802ee710ccfcec4a31e8158fb32e846e415
heitor2002 <montagnini.heitor@gmail.com>
2024-02-19 23:10:49 -0300

    feat: alteração de fotos.

--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -3,16 +3,36 @@ export default function AboutUs() {
     <section className="w-full py-20 bg-slate-100" id="aboutus">
       <div className="container max-w-3xl grid md:grid-cols-2 grid-cols-1">
         <div className="w-full flex justify-center items-center">
-            <div className="w-full max-w-xs h-96 back-image">
-                <img src="https://plus.unsplash.com/premium_photo-1689562473471-6e736b8afe15?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full object-cover custom-rounded" />
-            </div>
+          <div className="w-full max-w-xs h-96 back-image">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1689562473471-6e736b8afe15?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              className="w-full h-full object-cover custom-rounded"
+            />
+          </div>
         </div>
         <article className="md:mt-0 mt-16">
-            <h2 className="md:text-right text-center text-3xl text-orange-700 font-bold tracking-widest">Sobre nós...</h2>
-            <div className="w-full flex md:justify-end justify-center">
-                <div className="w-full max-w-32 h-1 bg-orange-600 mt-5"></div>
-            </div>
-            <p className="text-sm md:text-right text-center mt-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum, odio sed finibus viverra, dui dolor viverra urna, id fringilla orci nibh vel ligula. Aliquam semper mollis venenatis. Vivamus nec dapibus massa, nec pulvinar nulla. Aliquam lobortis lacus ac nunc hendrerit, et aliquet tortor vulputate. Nulla non dolor at sem blandit interdum. Donec sit amet euismod erat, id dictum odio. Sed scelerisque tempor ligula. Etiam id odio suscipit, congue felis ac, semper diam. In ultricies rutrum tellus. Etiam id magna id dolor rutrum hendrerit et vel elit.</p>
+          <h2 className="md:text-right text-center text-3xl text-orange-700 font-bold tracking-widest">
+            Sobre nós...
+          </h2>
+          <div className="w-full flex md:justify-end justify-center">
+            <div className="w-full max-w-32 h-1 bg-orange-600 mt-5"></div>
+          </div>
+          <p className="text-sm md:text-right text-center mt-10">
+            Há 5 anos, temos o orgulho de oferecer uma vasta variedade de
+            mármores, granitos, quartzitos e outras pedras naturais de todo o
+            mundo. Cada peça em nossa coleção é cuidadosamente selecionada,
+            refletindo a beleza atemporal e a singularidade que só as pedras
+            naturais podem oferecer.
+            <br></br>
+            <br></br>
+            O que nos diferencia na Marmoraria Decore não é apenas a
+            qualidade dos nossos produtos, mas também o compromisso com a
+            excelência em cada projeto. Nossa equipe trabalha em estreita
+            colaboração com cada cliente, desde a concepção até a instalação
+            final, para garantir que cada projeto atenda e exceda às
+            expectativas mais exigentes.
+          </p>
         </article>
       </div>
     </section>
--- src/app/components/SectionProjectsHome.tsx
+++ src/app/components/SectionProjectsHome.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
-import wave from "../../../public/wave.png";
-
 export default function SectionProjectsHome() {
+  const urlImages:string[] = [
+    "https://marmoraria-storage.s3.sa-east-1.amazonaws.com/tumulo-modelo-home.jpg",
+    "https://marmoraria-storage.s3.sa-east-1.amazonaws.com/pia-modelo-home-2.jpg",
+    "https://marmoraria-storage.s3.sa-east-1.amazonaws.com/pia-modelo-home.jpg",
+    "https://marmoraria-storage.s3.sa-east-1.amazonaws.com/tumulo-modelo-home-2.jpg",
+  ]
   return (
     <section className="bg-zinc-100 relative py-16">
       <div className="container">
@@ -10,31 +13,18 @@ export default function SectionProjectsHome() {
           <span className="italic font-bold text-orange-600">
             revestimentos
           </span>{" "}
-          de túmulos em granito, galerias e construções
+          de <span className="text-zinc-900 font-bold italic">túmulos, pias, soleiras, peitoris e lavatórios</span>
         </h2>
         <div className="bg-orange-500 w-full max-w-40 h-1 mb-20 mt-10"></div>
         <div className="container max-w-6xl grid grid-cols-2 gap-5 ">
           <div className="grid grid-cols-2 gap-4">
-            <img
-              src="https://images.unsplash.com/photo-1590227344930-9e75284f8de9?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            {urlImages.map(image => {
+              return <img
+              src={image}
               alt=""
-              className="hover:scale-105 duration-200"
+              className="hover:scale-105 duration-200 w-full h-full max-h-44 object-cover"
             />
-            <img
-              src="https://www.funerariaarce.com.br/wp-content/uploads/2019/02/tipos-de-tumulos-corpo-granito.jpg"
-              alt=""
-              className="hover:scale-105 duration-200"
-            />
-            <img
-              src="https://images.unsplash.com/photo-1512914890251-2f96a9b0bbe2?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              className="hover:scale-105 duration-200"
-            />
-            <img
-              src="https://plus.unsplash.com/premium_photo-1661963784453-1b96c2bb5aaa?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              className="hover:scale-105 duration-200"
-            />
+            })}
           </div>
           <div className="flex justify-end relative z-10">
             <img
