ec8dd0938a4f1ea3c15d8149cf3e6e5d9b61436a
heitor2002 <montagnini.heitor@gmail.com>
2024-03-01 20:46:54 -0300

    docs: mudando código de arquivo.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-const nextAuthOptions: NextAuthOptions = {
-  secret: process.env.SECRET_KEY_JSON_WEB_TOKEN,
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const response = await fetch("http://localhost:3000/api/users", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify({
-            email: credentials?.email,
-            password: credentials?.password,
-          }),
-        });
-
-        const user = await response.json();
-
-        if (user && response.ok) {
-          return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    async jwt({token, user}) {
-      user && (token.user = user)
-      return token
-    },
-
-    async session({session, token}){
-      session.user = token.user as any
-      return session
-    }
-  }
-  
-};
+import NextAuth from "next-auth";
+import { nextAuthOptions } from "./options";
 
 const handler = NextAuth(nextAuthOptions);
 
